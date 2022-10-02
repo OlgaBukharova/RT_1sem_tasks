ab5d8133502095d3de2c815e09af9e2432d2c24d
OlgaBukharova <bukharova.on@phystech.edu>
2022-10-02 16:48:35 +0300

    task 10 added

--- les2/task10_func_30.09.2022.cpp
+++ les2/task10_func_30.09.2022.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-using namespace std;
-
 char get_a_letter()
 {
     int c = 0;
@@ -9,18 +6,4 @@ char get_a_letter()
     while (!(((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z'))));
     
     return c;
-}
-
-char unleveling(char c)
-{
-    if (c >= 'a' && c <= 'z')
-    c += 'A' - 'a';
-    return c;
-}
-int main()
-{
-    for (int i = 0; i < 10; i++)
-        cout <<unleveling(get_a_letter());
-    cout <<endl;
-    return 0;
 }
